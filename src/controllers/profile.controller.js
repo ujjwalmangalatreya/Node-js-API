a2bf28b1a1787e6811f270c0adba61d05ef643a3
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-03-21 15:33:17 +0545

    Added auth middleware

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,18 +1,31 @@
 const { Sequelize } = require("sequelize");
 const { sequelize } = require("../db/db_connection.js");
 const { ApiResponse } = require("../utils/ApiResponse.js");
+const {ApiError} = require("../utils/ApiError.js")
 const Profile = require("../models/profile.models")(sequelize);
 
 
 module.exports = {
      profileLists: async (req, res) => {
           try {
+               const header = req.header['authorization']
+               if (!header) {
+                    return res.status(401).send(new ApiError(401,"Token no provided"))
+               }
                const profiles = await Profile.findAll();
                if (profiles.length === 0) {
                     return res.status(204).send(new ApiResponse(204, profiles, "No Profile Found"));
                }
                return res.status(200).send(new ApiResponse(200,profiles,"Success"));
           } catch (error) {
+               console.log(error);
+               return res.status(500).json({ error: 'Internal Server Error' });
+          }
+     },
+     profileByUserName: async (req, res) => {
+          try {
+               
+          }catch(error){
                console.log(error);
                return res.status(500).json({ error: 'Internal Server Error' });
           }
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const jwt = require("jsonwebtoken")
+const { ApiError } = require("../utils/ApiError")
+
+const authClientToken = async (req, res,next) => {
+     let token = req.headers['authorization']
+     console.log(token,"::::::::::::::::::::::")
+
+     if (!token) {
+          return res.status(401).send(new ApiError(401, "Auth Token Not provided"))
+          
+     }
+     next();
+     
+}
+
+module.exports = { authClientToken }
